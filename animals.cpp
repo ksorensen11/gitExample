f3e4a4f7375f6005dfd37871d3ec2f48870ca6ba
Kelsey A. Sorensen <soreke03@WS4579.(none)>
2013-11-19 14:10:22 -0600

    animals say what they should!

--- animals.cpp
+++ animals.cpp
@@ -26,13 +26,14 @@ public:
 int main (int argc, char const *argv[])
 {
     /* code */
-    vector<Animal> barn;
-    barn.push_back(Animal());
-    barn.push_back(Cow());
-    barn.push_back(Dog());
+  //needs to be vector of pointers to animals in order to ensure that the animals say the right thing 
+    vector<Animal*> barn;
+    barn.push_back(new Animal());
+    barn.push_back(new Cow());
+    barn.push_back(new Dog());
     
     for(auto a : barn) {
-        a.say();
+        a->say();
     }
     return 0;
 }
